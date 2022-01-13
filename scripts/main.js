53cabf03287a3d3707f0149bb958d0dd73ec09ac
jamestkusek <90416554+jamestkusek@users.noreply.github.com>
2022-01-13 22:14:16 +0000

    more progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onmouseover = function() {
+
+      myImage.setAttribute('src','images/jcooper2.jpg');
+}
+myImage.onmouseout = function() {
+  myImage.setAttribute("src","images/jcooper.jpg")
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('name, ya cunt?');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hello ' + myName + ".";
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello ' + storedName + ".";
+}
+myButton.onclick = function() {
+  setUserName();
+}
